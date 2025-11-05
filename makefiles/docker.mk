fa4c01c609b36f6182f99f59ed14979b0d4c81bd
victor castillo <victor.saul.ca@gmail.com>
2025-11-05 02:22:20 -0500

    config by on target

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -4,20 +4,24 @@ OS_TARGET ?= win
 
 # ====== Ruta de credenciales en host ======
 ifeq ($(OS_TARGET),linux)
-  PATH_AWS_CREDENTIALS_HOST ?= $(HOME)/.aws
-else
-  PATH_AWS_CREDENTIALS_HOST ?= ${PATH_AWS_CREDENTIALS}
-endif
-
-
-DOCKER_COMMAND = docker run --rm -i \
+  DOCKER_COMMAND = docker run --rm -i \
 	-v "$(CURDIR):/work" \
-	-v "$(PATH_AWS_CREDENTIALS_HOST):/root/.aws:ro" \
+	-v "$(HOME)/.aws:/root/.aws:ro" \
+	-e AWS_DEFAULT_REGION="$(REGION)" \
+	-e TF_VAR_owner=$(OWNER) \
+	-e TF_VAR_env=$(ENV) \
+	-e TF_VAR_region=$(REGION)
+
+else
+  DOCKER_COMMAND = docker run --rm -i \
+	-v "$(CURDIR):/work" \
+	-v "$(PATH_AWS_CREDENTIALS):/root/.aws:ro" \
 	-e AWS_DEFAULT_REGION="$(REGION)" \
 	-e AWS_PROFILE="$(AWS_PROFILE)" \
 	-e TF_VAR_owner=$(OWNER) \
 	-e TF_VAR_env=$(ENV) \
 	-e TF_VAR_region=$(REGION)
+endif
 
 build: ## Build image for development: make build.image
 	@docker build -t $(IMAGE) -f docker/Dockerfile.terraform .
@@ -32,7 +36,7 @@ show:
 	@ ${DOCKER_COMMAND} \
 	--entrypoint /bin/bash \
 	$(IMAGE) \
-	-c "aws configure list" 
+	-c "aws configure list"
 	
 
 
